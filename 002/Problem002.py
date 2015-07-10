d57439322229e2104e67b5ca9e6da363bbf0c630
Leeor Baskin <lbaskin@gmail.com>
2015-07-10 11:59:03 -0400

    Solution to Problem 2 in Python

---
+++ 002/Problem002.py
@@ -0,0 +1,20 @@
+''' 
+  * Problem002.py
+  *
+  * Project Euler: Problem 2
+  * 
+  * Problem: Find the sum of the even-valued terms
+  *          in the Fibonacci sequence under 4 million.
+  *
+'''
+
+def sumEvenFib(n):
+    sum = 0
+    first, second = 0, 1
+    while second < n:
+        first, second = second, first + second
+        if second % 2 == 0:
+            sum += second
+    return sum
+
+print sumEvenFib(4000000)
