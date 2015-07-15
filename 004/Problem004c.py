308d3420d2fa95fa2989ee3778ee77e49b24b640
Leeor Baskin <lbaskin@gmail.com>
2015-07-15 00:37:28 -0400

    Faster solution to Problem 4

---
+++ 004/Problem004c.py
@@ -0,0 +1,25 @@
+''' 
+  * Problem004c.py
+  *
+  * Project Euler: Problem 4
+  * 
+  * Problem: Find the largest palindrome made from the product of two 3-digit numbers.
+  *          
+  *
+'''
+
+import time
+start_time = time.time()
+
+is_palindrome = lambda x: str(x) == str(x)[::-1]
+
+
+for i in xrange(999,899,-1):
+    for j in xrange(999,899,-1):
+        if is_palindrome(i*j):
+            print i*j
+            break
+    if is_palindrome(i*j):
+        break
+
+print("--- %s seconds ---" % (time.time() - start_time))
