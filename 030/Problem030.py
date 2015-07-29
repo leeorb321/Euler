27397ae14eb8f7b179d8f498a4bb863076d57cd6
Leeor Baskin <lbaskin@gmail.com>
2015-07-28 23:01:19 -0400

    30

--- 030/Problem030.py
+++ 030/Problem030.py
@@ -1,7 +1,7 @@
 ''' 
-  * Problem029.py
+  * Problem030.py
   *
-  * Project Euler: Problem 29
+  * Project Euler: Problem 30
   * 
   * Problem: Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
   * 
@@ -11,9 +11,6 @@
 import time
 start_time = time.time()
 
-def is_sum_nth(num): return sum(int(str(num)[i])**5 for i in xrange(len(str(num)))) == num
-
-print sum([x for x in xrange(2,6*9**5) if is_sum_nth(x)])
-
+print sum([x for x in xrange(2,6*9**5) if sum(int(str(x)[i])**5 for i in xrange(len(str(x)))) == x]) 
 
 print("--- %s seconds ---" % (time.time() - start_time))
